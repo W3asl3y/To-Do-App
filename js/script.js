e7ac4f59cf573755ae3a6f29d5eee36c1660eea2
Heinrich <verwey41@gmail.com>
2023-03-30 22:18:25 +0200

    added an alert and background image

--- js/script.js
+++ js/script.js
@@ -3,7 +3,15 @@ const taskInput = document.getElementById("taskInput");
 const addTaskBtn = document.getElementById("addTaskBtn");
 const taskList = document.getElementById("taskList");
 
-// Add task
+// Displaying an alert message once when the website is launched
+window.alert("Welcome to your To-Do app! \n\n This is how the to-do app works: \n To mark that a task is complete click once on the task \n To delete a task double click on the task") 
+
+  
+
+// An array for all the tasks
+let tasks = [];
+
+// Adding a new task
 function addTask(e) {
   e.preventDefault();
   const task = taskInput.value;
@@ -12,15 +20,18 @@ function addTask(e) {
     li.innerText = task;
     taskList.appendChild(li);
     taskInput.value = "";
-  }
-}
 
-// Delete task
-function deleteTask(e) {
-  if (e.target.tagName === "LI") {
-    e.target.remove();
+    console.log(tasks);
   }
 }
+      
+
+// Deleting a task
+  function deleteTask(e) {
+    if (e.target.tagName === "LI") {
+      e.target.remove();
+    }
+  }
 
 // Mark task as done
 function markDone(e) {
@@ -29,7 +40,25 @@ function markDone(e) {
   }
 }
 
+
 // Event listeners
 addTaskBtn.addEventListener("click", addTask);
 taskList.addEventListener("dblclick", deleteTask);
 taskList.addEventListener("click", markDone);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
