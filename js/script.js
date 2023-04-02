6e0bbbd83028c2e59bc86916021eae5cf2a1db1b
Heinrich <verwey41@gmail.com>
2023-04-02 16:45:42 +0200

    Made changes to HTML and CSS

--- js/script.js
+++ js/script.js
@@ -3,8 +3,8 @@ const taskInput = document.getElementById("taskInput");
 const addTaskBtn = document.getElementById("addTaskBtn");
 const taskList = document.getElementById("taskList");
 
-// Displaying an alert message once when the website is launched
-window.alert("Welcome to your To-Do app! \n\n This is how the to-do app works: \n To mark that a task is complete click once on the task \n To delete a task double click on the task") 
+// // Displaying an alert message once when the website is launched
+// window.alert("Welcome to your To-Do app! \n\n This is how the to-do app works: \n To mark that a task is complete click once on the task \n To delete a task double click on the task") 
 
   
 
